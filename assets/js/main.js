bd60e261fdce26648553ba8c0fca94ac61fa9318
GioZaghi <vanni.freitas@hotmail.com>
2022-06-09 14:05:49 -0300

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,8 +25,55 @@ var backEnd = '#back-end';
 var bancoDeDados = '#banco-de-dados';
 
 
-function display(){
-    document.getElementById('btn-div').style.display = 'none';
+function changeDisplay(idNameChange) {
+    var idChangeMobile = document.getElementById("mobile");
+    var idChangeFront = document.getElementById("front-end");
+    var idChangeBack = document.getElementById("back-end");
+    var idChangeDBA = document.getElementById("dba");
+
+    if (idNameChange === 'mobile') {
+        idChangeFront.style.display = "none";
+        idChangeBack.style.display = "none";
+        idChangeDBA.style.display = "none";
+
+        if (idChangeMobile.style.display === "block") {
+            idChangeMobile.style.display = "none";
+        } else {
+            idChangeMobile.style.display = "block";
+        }
+    } else if (idNameChange === 'front-end') {
+        idChangeMobile.style.display = "none";
+        idChangeBack.style.display = "none";
+        idChangeDBA.style.display = "none";
+
+        if (idChangeFront.style.display === "block") {
+            idChangeFront.style.display = "none";
+        } else {
+            idChangeFront.style.display = "block";
+        }
+    }else if(idNameChange === 'back-end'){
+        idChangeMobile.style.display = "none";
+        idChangeFront.style.display = "none";
+        idChangeDBA.style.display = "none";
+
+        if (idChangeBack.style.display === "block") {
+            idChangeBack.style.display = "none";
+        } else {
+            idChangeBack.style.display = "block";
+        }
+    }else if(idNameChange === 'dba'){
+        idChangeMobile.style.display = "none";
+        idChangeFront.style.display = "none";
+        idChangeBack.style.display = "none";
+        
+        if (idChangeDBA.style.display === "block") {
+            idChangeDBA.style.display = "none";
+        } else {
+            idChangeDBA.style.display = "block";
+        }
+    }
+
+
 }
 
 //FRONT-END
